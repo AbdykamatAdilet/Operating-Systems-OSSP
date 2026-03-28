3516db13d79616b9a0ef92a96789903cd93e4548
Adik <mcpeadilet@gmail.com>
2026-03-28 14:30:00 +0300

    ossp

---
+++ practice_10/task1.asm
@@ -0,0 +1,24 @@
+section .data
+    msg db 'Hello, World!', 10, 13, 0 
+    msgLen equ $ - msg - 1
+
+section .text
+    extern GetStdHandle
+    extern WriteFile
+    extern ExitProcess
+    global _start
+
+_start:
+    push -11
+    call GetStdHandle
+    mov ebx, eax
+
+    push 0
+    push 0
+    push msgLen
+    push msg
+    push ebx
+    call WriteFile
+
+    push 0
+    call ExitProcess
