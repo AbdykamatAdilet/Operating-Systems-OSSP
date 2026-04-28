35d1f75f5925d90c112982f80d289fe997bf2a9d
Adik <mcpeadilet@gmail.com>
2026-04-29 00:37:15 +0300

    ossp

---
+++ practice_14/4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int n;
+
+    printf("Enter size: ");
+    scanf("%d", &n);
+
+    int *arr = (int*) malloc(n * sizeof(int));
+
+    if (arr == NULL) {
+        printf("Memory allocation failed!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = i * i;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    free(arr);
+    arr = NULL;
+
+    return 0;
+}
